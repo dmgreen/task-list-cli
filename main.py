fd9ac5c2c533a5d9875471274351bfec6e85e5df
Dalton Green <dmgreen93@gmail.com>
2026-09-11 16:00:03 -0700

    Add Ruff linter and match style suggestions

--- main.py
+++ main.py
@@ -2,18 +2,19 @@
 
 from sys import argv
 
-from task_list import task_list
+from task_list import TaskList
+
 
 def main(args) -> None:
-	"""Run the task list CLI."""
-	print("Task List CLI")
-	if len(args) > 2:
-		print("Error: Too many arguments provided.")
-		exit(1)
-	file_path = args[1] if len(args) == 2 else None
-	tl = task_list(file_path)
-	print(f"Loaded {len(tl.tasks)} tasks from {tl.file_path}.") 
+    """Run the task list CLI."""
+    print("Task List CLI")
+    if len(args) > 2:
+        print("Error: Too many arguments provided.")
+        exit(1)
+    file_path = args[1] if len(args) == 2 else None
+    tl = TaskList(file_path)
+    print(f"Loaded {len(tl.tasks)} tasks from {tl.file_path}.")
 
 
 if __name__ == "__main__":
-	main(argv)
+    main(argv)
